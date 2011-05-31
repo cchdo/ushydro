1c830b90817a4a158214e788daaf6df2e1f35824
Justin <fieldsjustin@gmail.com>
2011-05-31 11:17:29 -0400

    Removed the 'type' column from events, it's (kinda) a reserved word.  changed to 'categories'.  smoothed out event entry

--- app/controllers/staff/events_controller.rb
+++ app/controllers/staff/events_controller.rb
@@ -22,11 +22,18 @@ class Staff::EventsController < ApplicationController
 
   def create
     if params[:event]
+
+      cruise = Cruise.find(params[:event_form][:cruise])
+      parameter = Parameter.find(params[:event_form][:parameter])
       @new_event = Event.new(params[:event])
+      if c_p = CruisesParameter.find_or_create_by_parameter_id_and_cruise_id(parameter,cruise)
+        @new_event.cruises_parameter = c_p
+      end
+      puts "Looking and found :#{cruise} with p=#{parameter}\n"*2
       respond_to do |format|
         if @new_event.save
           flash[:notice] = 'Event was successfully created.'
-          format.html { redirect_to(events_path) }
+          format.html { redirect_to(staff_events_path) }
         else
           flash[:notice] = 'Event creation failed, please contact the USHydro staff.'
           format.html {redirect_to(root_path)}
--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110407164024) do
+ActiveRecord::Schema.define(:version => 20110531145202) do
 
   create_table "contacts", :force => true do |t|
     t.string   "first_name"
@@ -56,11 +56,11 @@ ActiveRecord::Schema.define(:version => 20110407164024) do
 
   create_table "events", :force => true do |t|
     t.integer  "cruises_parameter_id", :null => false
-    t.string   "type"
     t.datetime "date"
     t.text     "notes"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "category"
   end
 
   create_table "institutions", :force => true do |t|
