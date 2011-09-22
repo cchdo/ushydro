7e927814eb6ed7cc0b8970338433510b5af2e329
jfields <fieldsjustin@gmail.com>
2011-09-22 13:20:14 -0400

    Created REST service for cruises.

--- app/controllers/staff/cruises_controller.rb
+++ app/controllers/staff/cruises_controller.rb
@@ -32,10 +32,10 @@ class Staff::CruisesController < ApplicationController
   end
 
   def update
-    cruise = Cruise.find(params[:cruise])
+    @cruise = Cruise.find(params[:id])
     if @cruise.update_attributes(params[:cruise])
-      flash[:notice] = "Successfully updated event."
-      redirect_to staff_events_path
+      flash[:notice] = "Successfully updated cruise."
+      redirect_to staff_cruises_path
     else
       render :action => 'edit'
     end
@@ -45,28 +45,16 @@ class Staff::CruisesController < ApplicationController
   end 
 
   def create
-    if params[:event]
-      puts "\n\nPARAMS{}{}\n"
-      puts "#{params[:event]}\n"
-      cruise = Cruise.find(params[:event_form][:cruise])
-      parameter = Parameter.find(params[:event_form][:parameter])
-      @new_event = Event.new(params[:event])
-      puts "I expect these not to be 1:  #{cruise.id},  #{parameter.id} \n\n"*2
-      puts "NEWe 1:  #{@new_event.inspect}\n\n"*2
-      logger.info "I expect these not to be 1:  #{cruise.id},  #{parameter.id} \n\n"*2
-      if c_p = CruisesParameter.find_or_create_by_parameter_id_and_cruise_id(parameter.id,cruise.id)
-        @new_event.cruises_parameter = c_p
-      end
-      puts "Looking and found :#{cruise} with p=#{parameter}\n"*2
+    if params[:cruise]
+      @new_cruise = Cruise.new(params[:cruise])
       respond_to do |format|
-        if @new_event.save
-          flash[:notice] = 'Event was successfully created.'
-          format.html { redirect_to(staff_events_path) }
+        if @new_cruise.save
+          flash[:notice] = 'Cruise was successfully created.'
+          format.html { redirect_to(staff_cruises_path) }
         else
-          flash[:notice] = 'Event creation failed, please contact the USHydro staff.'
+          flash[:notice] = 'Cruise creation failed, please contact the USHydro staff.'
           format.html {redirect_to(root_path)}
         end
-
       end
     end
   end
